35421063c097834fcbfc1d1c7d869ebe6ccee2e2
Katie Li <katiesli16@gmail.com>
2015-07-01 10:35:46 -0400

    need to save these files for the transections

--- Movement/BTN_files/overwriteConcBTN.m
+++ Movement/BTN_files/overwriteConcBTN.m
@@ -1,25 +1,26 @@
-function overwriteConcBTN(datFile, baseFile, outFileName)
+function overwriteConcBTN(arr, baseFile, outFileName)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Intended to overwrite the concentration data in .btn file 
 % with matrix data from matlab 
 %
-% Katie Li
+% Katie Li, katiesli16@gmail.com
 % June 22, 2015
 % 
+% Modified July 1, 2015
+%
 % Stroud Water Research Center
-% University of Delaware
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Set up constants, variables used in the function
-% datFile: a Matlab matrix containing concentration data in the right
-% orientation
-% baseFile: origin BTN file used by SEAWAT
+% arr: array containing the values to overwrite in correct orientation
+% baseFile: original BTN file used by SEAWAT
 % outFileName: name of the output file 
 
     if nargin < 3   % current default values for inputs
         datFile = 'C6_1.mat';
-        baseFile = 'OldTest.btn';
-        outFileName = 'Test.btn'; 
+        arr = matfile(datFile);  arr = arr.C;% extract variable
+        baseFile = 'Test.btn';
+        outFileName = 'NewTest.btn'; 
     end 
     
     HEADER_TEXT = ' 103'; %used to match on a header
@@ -29,11 +30,9 @@ function overwriteConcBTN(datFile, baseFile, outFileName)
 
     %% Start of script
     fout = fopen(outFileName, 'wt');
-    mat = matfile(datFile); 
-    cMat = mat.C; % extract variable
     %columns from left to right go sea to landward
     % solute concentrations should be in ML-3
-    [rows, cols] = size(cMat); %save size of salinity matrix
+    [rows, cols] = size(arr); %save size of salinity matrix    
     fid = fopen(baseFile,'r');
 
     tic; 
@@ -48,14 +47,16 @@ function overwriteConcBTN(datFile, baseFile, outFileName)
     k = strsplit(tline, ' '); %separate them by line
     nBlocks = str2double(cell2mat(k(2))); %conversion into comparable format
     nCells = str2double(cell2mat(k(3)));
-
+    
+    disp(nCells);     disp(nBlocks); disp(fid);
+    
     if (nCells ~= cols || nBlocks ~= rows) %wrong input values/dimensions
         disp('Wrong size inputs'); % matricies do not align to correct sizes
         return;
     end
 
 
-    time = datestr(now, 29) %get current date
+    time = datestr(now, 29); %get current date
     %Write header for SEAWATBTN file
     str = sprintf('#BTN file created using MATLAB on %s', time);
     fprintf(fid,'%s\r\n',str);  
@@ -74,8 +75,6 @@ function overwriteConcBTN(datFile, baseFile, outFileName)
            h  = h+ 1; % increment header
         end
     end
-
-
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Start to copy in new data from datFile, ignoring values from OldTest.btn
     % The strange movement and modifications in here is due to the offset
@@ -86,11 +85,11 @@ function overwriteConcBTN(datFile, baseFile, outFileName)
           if  (j == (nCells+1))        % do not modify header
         	fprintf(fout, '%s', tline);
           else %rewrite data
-            newConc = cMat(i, j); % newConc is the value to be written
+            newConc = arr(i, j); % newConc is the value to be written
             fprintf(fout, '%d\n', newConc); 
           end
           tline = fgets(fid); 
-    	end
+        end
     end
 
 
