b4cb14b455138440c4a38ed8ea2111fe9bfdbd0d
Katie Li <katiesli16@gmail.com>
2015-07-14 16:25:48 -0400

    completed file right before the runtime starts...

--- workspace/run.bat
+++ workspace/run.bat
@@ -3,14 +3,12 @@ ECHO Purpose: determine optimum discretization
 
 
 REM Set which discretizations to run
-set disc = disc134_800 disc134_1600 disc268_800 disc268_1600 disc536_1600
-set profiles = profile5.30.mat profile5.21.mat
-(for %%n in (%disc%) do (
-  echo %%n
-  (for %%hk in (%profiles%) do (
-    echo %%hk
-    CD  C:\User\Admin\Desktop\KatieLi\Seawater\workspace\%%n\basefiles\%%hk
-    REM C:\Users\Student\DOCUME~1\SWT_V4~1\SWT_V4~1\exe\swt_v4.exe Test.nam
+set DISC= (disc134_800 disc134_1600 disc268_800 disc268_1600 disc536_800)
+set PROFILES= (profile5.30.mat profile5.21.mat)
+(for %%n in %DISC% do (
+  (for %%h in %PROFILES% do (
+    CD C:\Users\Admin\Desktop\KatieLi\Seawater\workspace\%%n\%%h
+    C:\Users\Student\DOCUME~1\SWT_V4~1\SWT_V4~1\exe\swt_v4.exe Test.nam
   )
   )
 )
