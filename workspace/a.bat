576ac1a51394bb121d0ddd6cd383054360c1bbda
Katie Li <katiesli16@gmail.com>
2015-07-27 12:01:43 -0400

    finished running for 50k, correct head files and everything

---
+++ workspace/a.bat
@@ -0,0 +1,6 @@
+ECHO Purpose: determine optimum discretization
+
+REM Set which discretizations to run
+CD C:\Users\Admin\Desktop\KatieLi\Seawater\workspace\disc134_800\profile5.30.mat
+start C:\PROGRA~2\SWT_V4~1\exe\SWT_V4~1.EXE Test.nam
+
---
+++ workspace/disc134_800/Test/SEAWAT.bat
@@ -0,0 +1,2 @@
+start C:\PROGRA~2\SWT_V4~1\exe\SWT_V4~1.EXE Test.nam
+
